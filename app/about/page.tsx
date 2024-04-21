d5b14f08112edceb57d72d3f605d138bd2c5a4e4
gangbeomsu <kbs21c1987@gmail.com>
2024-04-21 20:52:26 +0900

    메타데이터 추가

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '',
+  description: '',
+};
+
+const AboutPAge = () => {
+  return <div>AboutPAge</div>;
+};
+
+export default AboutPAge;
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,74 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
+  // base
+  metadataBase: new URL('https://example.com'),
+
+  // 기본
   title: {
-    default: 'NEXT JS SEO',
-    template: '%s | NEXT JS SEO',
+    default: '',
+    template: '%s - ',
   },
-  description: 'NEXT JS SEO',
+  description: '',
+
+  // robots
+  robots: {
+    index: true,
+    follow: true,
+  },
+
+  // og
+  openGraph: {
+    type: 'website',
+    locale: 'ko_KR',
+    url: '',
+    siteName: '',
+    title: '',
+    description: '',
+    images: [
+      {
+        url: '',
+        width: 800,
+        height: 600,
+        alt: '',
+      },
+    ],
+  },
+
+  twitter: {
+    card: 'summary_large_image',
+    title: '',
+    description: '',
+    site: '',
+    creator: '',
+    images: [
+      {
+        url: '',
+        width: 800,
+        height: 600,
+        alt: '',
+      },
+    ],
+  },
+  // 기타
+  formatDetection: {
+    address: true, // 주소 형식을 감지하고 링크를 생성합니다.
+    email: true, // 이메일 형식을 감지하고 링크를 생성합니다.
+    telephone: true, // 전화번호 형식을 감지하고 링크를 생성합니다.
+    date: true, // 날짜 형식을 감지하고 링크를 생성합니다.
+    url: true, // URL 형식을 감지하고 링크를 생성합니다.
+  },
+
+  generator: 'Next.js',
+  applicationName: 'Next.js',
+
+  authors: [
+    { name: '', url: '' },
+    { name: '', url: '' },
+  ],
+  creator: '',
+  publisher: '',
+  keywords: [''],
 };
 
 export default function RootLayout({
